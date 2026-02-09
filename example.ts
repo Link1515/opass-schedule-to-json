eeb79b7f088032c5fd7ce5fd85b9dde5299e2506
Link1515 <terrylin1515@gmail.com>
2026-02-09 15:40:55 +0800

    docs: add example

---
+++ example.ts
@@ -0,0 +1,9 @@
+import fs from 'fs/promises'
+import { scheduleToJson } from './index'
+
+const schedule = await scheduleToJson({
+  apiKey: '<your-api-key>',
+  spreadsheetId: '<your-spreadsheet-id>'
+})
+
+fs.writeFile('./schedule.json', JSON.stringify(schedule, null, 2))
