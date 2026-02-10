d639efa80ee02982c97734659894313ee8b1bd4d
Link1515 <terrylin1515@gmail.com>
2026-02-10 15:10:15 +0800

    chore: update example

--- README.md
+++ README.md
@@ -6,5 +6,7 @@
 const schedule = await scheduleToJson({
   apiKey: '<your-api-key>',
   spreadsheetId: '<your-spreadsheet-id>',
+  avatarBaseUrl: 'https://example.com/',
+  defaultAvatar: 'https://example.com/defaultAvatar.jpg',
 })
 ```
--- example.ts
+++ example.ts
@@ -4,6 +4,8 @@ import { scheduleToJson } from './index'
 const schedule = await scheduleToJson({
   apiKey: '<your-api-key>',
   spreadsheetId: '<your-spreadsheet-id>',
+  avatarBaseUrl: 'https://example.com/',
+  defaultAvatar: 'https://example.com/defaultAvatar.jpg',
 })
 
 fs.writeFile('./schedule.json', JSON.stringify(schedule, null, 2))
